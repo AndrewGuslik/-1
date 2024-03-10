6ada7729be387fac7233394be34a5f0c1d154b4e
AndrewGuslik <mamon_vrn@mail.ru>
2024-03-10 22:25:37 +0300

    Добавьте файлы проекта.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,90 @@
+
+using ДЗЗ1;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void test_date_dif_1()
+        {
+            Class1 dtl = new Class1();
+
+            DateTime date1 = new DateTime(2024, 11, 23, 22, 45, 30);
+            DateTime date2 = new DateTime(2023, 9, 10, 15, 42, 20);
+            DateTime exp = new DateTime(2, 3, 17, 7, 3, 10);
+
+
+            DateTime result = dtl.date_dif(date1, date2);
+
+            Assert.AreEqual(exp, result);
+        }
+
+        [TestMethod]
+        public void test_date_dif_2()
+        {
+            Class1 dtl = new Class1();
+
+            DateTime date1 = new DateTime(2024, 11, 23, 22, 45, 30);
+            DateTime date2 = new DateTime(2023, 9, 10, 15, 42, 20);
+            DateTime exp = new DateTime(2, 3, 17, 7, 3, 10);
+
+            DateTime result = dtl.date_dif(date2, date1); // Меняем местами аргументы
+
+            Assert.AreEqual(exp, result);
+        }
+
+        [TestMethod]
+        public void test_isYearLeap_true()
+        {
+            Class1 dtl = new Class1();
+
+            DateTime date1 = new DateTime(2024, 11, 23, 22, 45, 30);
+            bool exp = true;
+
+            bool result = dtl.isYearLeap(date1.Year);
+
+            Assert.AreEqual(exp, result);
+        }
+
+        [TestMethod]
+        public void test_isYearLeap_false()
+        {
+            Class1 dtl = new Class1();
+
+            DateTime date1 = new DateTime(2023, 11, 23, 22, 45, 30);
+            bool exp = false;
+
+            bool result = dtl.isYearLeap(date1.Year);
+
+            Assert.AreEqual(exp, result);
+        }
+
+        [TestMethod]
+        public void test_timesOfDay_night()
+        {
+            Class1 dtl = new Class1();
+
+            DateTime date1 = new DateTime(2023, 11, 23, 2, 45, 30);
+            string exp = "ночь";
+
+            string result = dtl.timesOfDay(date1);
+
+            Assert.AreEqual(exp, result);
+        }
+
+        [TestMethod]
+        public void test_formatDate()
+        {
+            Class1 dtl = new Class1();
+
+            DateTime date1 = new DateTime(2024, 11, 23, 22, 45, 30);
+            string exp = "23 ноября 2024 г. 22:45";
+
+            string result = dtl.formatDate(date1, "f");
+
+            Assert.AreEqual(exp, result);
+        }
+    }
+}
